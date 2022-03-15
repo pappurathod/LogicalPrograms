826979b819247181107950d476985f0dbd093169
pappurathod <prathod4545@gmail.com>
2022-03-15 10:55:12 +0530

    Perfect Number

---
+++ LogicalPrograms/LogicalPrograms/PerfectNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class PerfectNumber
+    {
+
+        public void Perfect()
+        {
+            int sum = 0;
+            Console.WriteLine("enter number");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            if (sum == num)
+            {
+                Console.WriteLine("{0} is perfect number", num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not perfect number", num);
+            }
+        }
+    }
+}
--- LogicalPrograms/LogicalPrograms/Program.cs
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -4,9 +4,24 @@
     {
         public static void Main(string[] args)
         {
-            FibonacciSeries fiboseries = new FibonacciSeries();
-            fiboseries.Fibo();
+            Console.WriteLine("Enter Your Choice");
+            Console.WriteLine("1 = Fibonacci Series");
+            Console.WriteLine("2 = Perfect Number");
+            int ch = int.Parse(Console.ReadLine());
 
+            switch (ch)
+            {
+                case 0:
+                    FibonacciSeries fiboseries = new FibonacciSeries();
+                    fiboseries.Fibo();
+                    break;
+
+                case 1:
+                    PerfectNumber perfectnum = new PerfectNumber();
+                    perfectnum.Perfect();
+                    break;
+
+            }
         }
     }
 }
